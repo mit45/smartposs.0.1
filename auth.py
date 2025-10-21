0ec43c65547667ecf69c413369d62029f0741d6f
Ümit Topuz <umittopuzg@gmail.com>
2025-10-22 00:17:24 +0300

    JWT token üretmek

--- auth.py
+++ auth.py
@@ -1,4 +1,10 @@
 import json
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "super_secret_key_123"  # Gerçek sistemde .env dosyasına yazılacak
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def authenticate_user(username, password):
     with open("users.json", "r", encoding="utf-8") as f:
@@ -8,3 +14,13 @@ def authenticate_user(username, password):
         if user["username"] == username and user["password"] == password:
             return True
     return False
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, HTTPException
-from auth import authenticate_user
+from datetime import timedelta
+from auth import authenticate_user, create_access_token
 
 app = FastAPI()
 
 @app.post("/login")
 def login(username: str, password: str):
     if authenticate_user(username, password):
-        return {"message": "Giriş başarılı", "token": "örnek_token_123"}
+        access_token_expires = timedelta(minutes=60)
+        access_token = create_access_token(
+            data={"sub": username}, expires_delta=access_token_expires
+        )
+        return {"message": "Giriş başarılı", "token": access_token}
     else:
         raise HTTPException(status_code=401, detail="Kullanıcı adı veya şifre hatalı")
